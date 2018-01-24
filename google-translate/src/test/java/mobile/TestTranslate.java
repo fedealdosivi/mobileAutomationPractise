a8bca7ed144c73ed36542942a599f6a25d5509b8
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-01-24 12:30:46 -0300

    few changes

--- google-translate/src/test/java/mobile/TestTranslate.java
+++ google-translate/src/test/java/mobile/TestTranslate.java
@@ -14,8 +14,6 @@ public class TestTranslate extends MobileTest<translateHome> {
 
     @Override
     protected void setCapabilities(DesiredCapabilities desiredCapabilities) {
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("deviceName", "emulator-5554");
         desiredCapabilities.setCapability("appPackage","com.google.android.apps.translate");
         desiredCapabilities.setCapability("appActivity", "com.google.android.apps.translate.TranslateActivity");
     }
